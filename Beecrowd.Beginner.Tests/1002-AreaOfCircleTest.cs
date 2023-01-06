c9da5469290d0828cb1709df53bc6b730e310dd5
Roohallah <78079704+rhazizi@users.noreply.github.com>
2023-01-06 16:39:08 +0300

    P1002: Solution for "Area Of Circle"

---
+++ Beecrowd.Beginner.Tests/1002-AreaOfCircleTest.cs
@@ -0,0 +1,25 @@
+using Beecrowd.Beginner.Solutions;
+using Xunit;
+
+namespace Beecrowd.Beginner.Tests;
+
+[Collection("Sequential")]
+public class AreaOfCircleTest
+{
+    [Theory]
+    [InlineData(2.00, "A=12.5664")]
+    [InlineData(100.64, "A=31819.3103")]
+    [InlineData(150.00, "A=70685.7750")]
+    public void CallAreaMethod_ShowCapitalAEqualAreaOfCircle(double number1, string expectedResult)
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        var stringReader = new StringReader(number1.ToString());
+        Console.SetIn(stringReader);
+
+        AreaOfCircle.CalculateArea();
+
+        var result = stringWriter.GetStringBuilder().ToString().Trim().Replace(",", ".");
+        Assert.Equal(expectedResult, result);
+    }
+}
