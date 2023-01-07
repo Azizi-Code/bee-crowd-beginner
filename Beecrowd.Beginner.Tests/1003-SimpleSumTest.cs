184e0e775811666a6b6b32f5ed8bf3e9a668d08a
Roohallah <Azizi.Code@gmail.com>
2023-01-07 15:19:51 +0300

    P-1003: Solution for "Simple Sum"

---
+++ Beecrowd.Beginner.Tests/1003-SimpleSumTest.cs
@@ -0,0 +1,24 @@
+using Beecrowd.Beginner.Solutions;
+using Xunit;
+
+namespace Beecrowd.Beginner.Tests;
+
+public class SimpleSumTest
+{
+    [Theory]
+    [InlineData(30, 10, "SOMA = 40")]
+    [InlineData(-30, 10, "SOMA = -20")]
+    [InlineData(0, 0, "SOMA = 0")]
+    public void CallSumMethod_ShowCapitalXEqualSumOfTwoInputNumber(int number1, int number2, string expectedResult)
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        var stringReader = new StringReader(number1 + Environment.NewLine + number2);
+        Console.SetIn(stringReader);
+
+        SimpleSum.CalculateSum();
+
+        var result = stringWriter.GetStringBuilder().ToString().Trim();
+        Assert.Equal(expectedResult, result);
+    }
+}
